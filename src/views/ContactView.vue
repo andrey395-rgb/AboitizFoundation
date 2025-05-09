3ff761fdb590ece7541af783c4649739689f52be
andrey395-rgb <chubbyramirez103@gmail.com>
2025-05-09 16:39:56 +0800

    contactView setup

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,45 +1,47 @@
 <template>
   <div class="home">
     <div class="first-home">
-      <div class="message-container">
-        <h1>Location Map</h1>
+      <div class="left-container">
+        <h1>Contact Us</h1>
+        <h2>Reach and Contact us for a future that’s brighter than ever</h2>
+        <div class="callus-container">
+        <h1>Call Us</h1>
+        <div class="inner-icons"> 
+          <img src="/src/components/icons/Frame.svg" alt=""><h1>#000-000-000</h1>
+        </div>
+       </div>
         <iframe
-          src="https://www.google.com/maps/d/embed?mid=19M5SYCrApuPKYSXpltWhlaCNmfaCfWM&ll=12.873035405041499%2C121.77401750000001&z=6"
-          width="600"
-          height="500"
+          src="https://www.google.com/maps/d/u/0/embed?mid=1NBYf_O7PJTmSs1sjaknUWhbYFhgZC0w&ehbc=2E312F"
+          width="400"
+          height="400"
           style=" top: -50px; border: 0"
           loading="lazy"
         ></iframe>
+       
 
-        <!-- 
-        <iframe
-          width="600"
-          height="450"
-          style="border: 0"
-          loading="lazy"
-          allowfullscreen
-          referrerpolicy="no-referrer-when-downgrade"
-          src="https://www.google.com/maps/d/embed?mid=YOUR_CUSTOM_MAP_ID"
-        >
-        </iframe> -->
+       <div class="visitus-container">
+        <div class="inner-icons">
+
+        </div>
+       </div>
       </div>
-      <div class="contact-container">
+      <div class="right-container">
         <section id="contact">
-          <h1 class="sectionTitle">Work with me!</h1>
+          <!-- <h1 class="sectionTitle">Work with me!</h1> -->
           <form action="https://formspree.io/f/mqaqbzvl" method="POST">
             <div class="formGroup">
-              <label for="name" hidden> Name </label>
-              <input type="text" name="name" id="name" placeholder="Name" required />
+              <label for="name" style="color: white;"> Name </label> <br>
+              <input type="text" name="name" id="name" style="width: 45vh; border-radius: 2vh; border-style: none;" required />
             </div>
             <div class="formGroup">
-              <label for="email" hidden> Email </label>
-              <input type="text" name="email" id="email" placeholder="Email" required />
+              <label for="email" style="color: white;"> Email </label> <br>
+              <input type="text" name="email" id="email"  style="width: 45vh; border-radius: 2vh; border-style: none;" required />
             </div>
             <div class="formGroup">
-              <label for="message" hidden> Message </label>
-              <textarea name="message" id="message" placeholder="Message" required></textarea>
+              <label for="message" style="color: white;"> Message </label> <br>
+              <textarea name="message" id="message" style="width: 45vh; min-height: 30vh; max-width: 45vh; border-radius: 2vh; border-style: none;" required></textarea>
             </div>
-            <input class="hover btn" type="submit" value="Submit" />
+            <input class="hover btn" type="submit" value="Submit" style="padding: 1vh; border-radius: 1vh; border-style: none; color: #dc1b28; background-color: #FFFFFF;" />
           </form>
         </section>
       </div>
@@ -48,6 +50,13 @@
 </template>
 
 <style scoped>
+.inner-icons{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  /* justify-content: center; */
+  margin-top: 1rem;
+}
 .first-home {
   width: 100%;
   padding: 4rem 4rem 0 4rem;
@@ -58,7 +67,7 @@
   justify-content: center;
 }
 
-.contact-container {
+.right-container {
   background-color: #dc1b28;
   display: flex;
   justify-content: center;
@@ -68,10 +77,10 @@
   padding: 2rem;
 }
 
-.message-container {
+.left-container {
   width: 50%;
   height: 100%;
-  padding-top: 10rem;
+  /* padding-top: 10rem; */
 }
 
 .home {
